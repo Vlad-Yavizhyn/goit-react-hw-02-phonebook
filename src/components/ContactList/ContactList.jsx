94e0edefc985ee86af3801c118f14ee685b010d6
Vladlen <91382235+Vlad-Yavizhyn@users.noreply.github.com>
2022-08-18 00:29:29 +0300

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,8 +3,9 @@ import { ContactListItem } from "./ContactListItem";
 export function ContactList({contacts, handleDeleteContact}) {
     return (
     <ul>
-        {contacts.map(({id, name, number}) => <ContactListItem key={id} name={name} number={number} handleDeleteContact = {handleDeleteContact}/>
-        )}
+        {contacts.map(({id, name, number}) => (
+        <ContactListItem key={id} name={name} number={number} handleDeleteContact = {handleDeleteContact}/>
+        ))}
     </ul>
     );
 }
